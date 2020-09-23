758b290f1d5c3140fb526214b06e659611e562dd
Ukko Kamula <kamulaukko@gmail.com>
2020-09-23 08:25:19 +0300

    pieniä design muutoksia

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             <div className="index-tertiary">Hold the color</div>
             <div className="index-tertiary">Don't over invest</div>
             <div className="index-secondary">Don't design too much</div>
-            <div className="index-tertiary">Work in cycles</div>
+            <div className="index-tertiary">Work in git remote add origin</div>
             <div className="index-tertiary">Be a pessimist</div>
             <div className="index-secondary">Choose a personality</div>
             <div className="index-tertiary">Be a pessimist</div>
@@ -87,7 +87,15 @@ function App() {
 
         Consectetur esse consequat pariatur ex id ex sunt cupidatat laboris cillum. Proident officia dolore ea exercitation incididunt ex do nostrud cupidatat deserunt. Minim occaecat consectetur velit exercitation laborum aliquip proident laborum. Qui mollit nisi esse proident excepteur et pariatur dolore reprehenderit deserunt adipisicing excepteur proident voluptate. Dolore dolore reprehenderit excepteur irure d 
         </p>
-        <h1>Refactoring UI</h1>
+        <h2>Detail comes later</h2>
+        <p>Et et qui cillum anim aliquip. Amet nisi ipsum magna qui aute laboris eiusmod ad nisi excepteur veniam. Non sint cillum magna incididunt ipsum exercitation sunt incididunt adipisicing quis voluptate anim reprehenderit aliquip. Irure deserunt adipisicing incididunt fugiat magna.
+
+        Aliqua commodo ut esse sunt dolore ea ea laborum fugiat. Excepteur velit cillum nulla nostrud. Aute magna nulla incididunt veniam.
+
+        Ipsum do tempor culpa fugiat nulla labore veniam do minim velit eu. Mollit fugiat ipsum laboris veniam occaecat qui aliqua cupidatat consectetur consectetur in ipsum sint. Consectetur adipisicing pariatur deserunt ullamco occaecat veniam reprehenderit esse laboris nostrud excepteur duis eiusmod. Sint sit laboris irure magna. Culpa ad non qui laborum id anim ea et ea aliquip exercitation proident nisi eu.
+
+        Ex irure anim dolor qui Lorem anim amet incididunt mollit exercitation minim pariatur exercitation cillum. Veniam commodo ut veniam incididunt qui mollit ullamco sint. Mollit et et laboris dolor tempor. Sint laboris quis dolor sit.</p>
+       
         <p>Ex nulla in in volupdtate quis proident voluptate enim excepteur esse. Dolor quis nostrud adipisicing deserunt qui officia excepteur. Duis anim cillum reprehenderit ipsum Lorem occaecat. Velit deserunt consequat fugiat proident consequat. Aliqua laboris velit ullamco elit excepteur laboris esse cupidatat elit. Duis qui laborum do reprehenderit.
 
         Eiusmod ullamco duis enim Lorem. Sint do incididunt sint magna reprehenderit. Velit cupidatat nostrud exercitation magna commodo cupidatat Lorem enim eiusmod velit velit irure.
